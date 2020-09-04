01ea461eb09d01e4de2bbeb535159e3f6d75228b
tianxuanxuan <xx_tian189@163.com>
2020-09-04 18:00:19 +0800

    1.集成axios实现get请求 2.el-table数据展示

--- src/main.js
+++ src/main.js
@@ -4,9 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import ElementUI from 'element-ui'
+import '../node_modules/element-ui/lib/theme-chalk/index.css'
+import axios from '../node_modules/axios'
+import qs from '../node_modules/qs'
+
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
+Vue.prototype.qs = qs
 
 /* eslint-disable no-new */
+
+Vue.use(ElementUI)
 new Vue({
   el: '#app',
   router,
